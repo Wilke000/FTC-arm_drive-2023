237482d596b9502787078ed94932941d4ac87160
Wilke000 <carterwilke@gmail.com>
2023-12-12 18:43:14 -0500

    update to working tele-op code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmFunctions.java
@@ -8,14 +8,14 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 public class ArmFunctions {
     private DcMotorEx armMotor;
-    private Servo armRotServo, clawServo /*planeLauncher*/;
+    private Servo armRotServo, clawServo, planeLauncher;
     public ArmFunctions(HardwareMap hardwareMap) {
         armMotor = hardwareMap.get(DcMotorEx.class, "armMotor");
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         armRotServo = hardwareMap.get(Servo.class, "armRotServo");
         clawServo = hardwareMap.get(Servo.class, "clawServo");
-        //planeLauncher = hardwareMap.get(Servo.class, "planeLauncher");
+        planeLauncher = hardwareMap.get(Servo.class, "planeLauncher");
     }
     double clawPos;
     double clawRotPos;
@@ -26,14 +26,16 @@ public class ArmFunctions {
             armRotServo.setPosition(0.5);
         }
     }
-    /*
-    public void planeLaunch(boolean GP2LB, boolean GP2RB) {
+
+    public void planeLaunch(boolean GP2START, boolean GP2LB, boolean GP2RB) {
         PLpos = planeLauncher.getPosition();
-        if (GP2LB == GP2RB) {
-            planeLauncher.setPosition(0  fix later );
+        if (GP2START) {
+            planeLauncher.setPosition(0.5); // initial
+        } else if (GP2RB && GP2LB) {
+            planeLauncher.setPosition(0.1); // launched
         }
     }
-    */
+
     public void clawRot(boolean gamepad1x, boolean gamepad1b) {
         if (gamepad1x) {
             armRotServo.setPosition(0.5 /* 0 degrees */); // down
